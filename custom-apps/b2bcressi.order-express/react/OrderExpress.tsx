8a3c527117e42f6089cbd5e543b35c72d143ecab
Gabriel Leocadio <gabrielsilva94@gmail.com>
2023-10-04 22:39:50 -0300

    feat: estrutura e estilo do app pedido express

---
+++ custom-apps/b2bcressi.order-express/react/OrderExpress.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrderExpress: StorefrontFunctionComponent = () => {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  )
+}
+
+export default OrderExpress
