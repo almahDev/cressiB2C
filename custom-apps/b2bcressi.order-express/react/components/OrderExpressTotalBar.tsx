e582bb3088d50d428ee90c6e7d3b3a39cde25bd3
Gabriel Leocadio <gabrielsilva94@gmail.com>
2023-10-14 01:57:11 -0300

    feat: estrutura e estilo dos apps

--- custom-apps/b2bcressi.order-express/react/components/OrderExpressTotalBar.tsx
+++ custom-apps/b2bcressi.order-express/react/components/OrderExpressTotalBar.tsx
@@ -1,12 +1,28 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useContext, useState } from 'react'
 import { FormattedCurrency } from 'vtex.format-currency'
 import { Spinner } from 'vtex.styleguide'
+import type { Session, SessionResponse } from 'vtex.render-runtime'
 
 import styles from '../styles.css'
 import { OrderExpressContext } from '../contexts/OrderExpressContext'
+import useSessionResponse from '../hooks/useSessionResponse'
 
 interface OrderExpressTotalBarProps {
   sticky?: boolean
+}
+
+type SessionExtended = Session & {
+  namespaces: {
+    store: {
+      countryCode: {
+        value: string
+      }
+      channel: {
+        value: string
+      }
+    }
+  }
 }
 
 const OrderExpressTotalBar = ({
@@ -14,39 +30,33 @@ const OrderExpressTotalBar = ({
 }: OrderExpressTotalBarProps) => {
   const [loading, setLoading] = useState<boolean>(false)
   const { selectedItems, globalLoading } = useContext(OrderExpressContext)
-  console.log(
-    '🚀 ~ file: OrderExpressTotalBar.tsx:15 ~ selectedItems:',
-    selectedItems
-  )
+  const sessionResponse: SessionResponse | undefined = useSessionResponse()
+  const channel =
+    (sessionResponse as SessionExtended)?.namespaces?.store?.channel?.value ||
+    '1'
 
   const subtotal = selectedItems?.reduce((acc, item) => {
     const price = item.price ?? 0
-    const quantity = item.quantity ?? 1
+    const quantity = Math.max(1, item.quantity ?? 1)
 
     return acc + price * quantity
   }, 0)
 
-  console.log(
-    '🚀 ~ file: OrderExpressTotalBar.tsx:31 ~ subtotal ~ subtotal:',
-    subtotal
-  )
-
   // const isEmpty = selectedItems?.length === 0 || subtotal === 0
-  // console.log('🚀 ~ file: OrderExpressTotalBar.tsx:33 ~ isEmpty:', isEmpty)
 
   const addtoCart = () => {
     setLoading(true)
 
     const linkItems = selectedItems?.reduce(
       (currentItems: string, item: any) =>
-        `${currentItems}sku=${item?.id}&qty=${item?.quantity || 1}&seller=${
-          item?.seller
-        }&`,
+        `${currentItems}sku=${item?.id}&qty=${Math.max(
+          1,
+          item.quantity ?? 1
+        )}&seller=${item?.seller}&`,
       ''
     )
 
-    // window.location.href = `/checkout/cart/add?${linkItems}sc=${channel}`
-    window.location.href = `/checkout/cart/add?${linkItems}sc=1`
+    window.location.href = `/checkout/cart/add?${linkItems}sc=${channel}}`
   }
 
   return (
@@ -55,7 +65,7 @@ const OrderExpressTotalBar = ({
         sticky ? `${styles.totalBarSticky}` : ''
       } w-100 flex items-center justify-end`}
     >
-      <div className={`${styles.totalBarSubtotal} t-body`}>
+      <div className={`${styles.totalBarSubtotal} t-body flex items-center`}>
         Subtotal:{' '}
         <span className={`${styles.totalBarSubtotalPrice} t-body`}>
           {globalLoading ? (
---
+++ custom-apps/b2bcressi.order-express/react/modules/session.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { SessionPromise } from 'vtex.render-runtime'
+
+export function getSession() {
+  return window &&
+    (window as any).__RENDER_8_SESSION__ &&
+    (window as any).__RENDER_8_SESSION__.sessionPromise
+    ? ((window as any).__RENDER_8_SESSION__
+        .sessionPromise as Promise<SessionPromise>)
+    : null
+}
